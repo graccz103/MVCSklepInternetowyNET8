e9e72e3b531dd5a70590f35ea5d8882a462dde93
graccz103 <149792016+graccz103@users.noreply.github.com>
2024-10-28 22:48:35 +0100

    Add product fix (big)
    added option to add product by also uploading images to it, fixded all CRUD to work with new Product and ProductViewModel

---
+++ MVCSklepInternetowyNET8/Migrations/20241028214151_AddImageColumnsToProducts.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCSklepInternetowyNET8.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageColumnsToProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "LargeImage",
+                table: "Products",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Thumbnail",
+                table: "Products",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LargeImage",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ MVCSklepInternetowyNET8/Models/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+public class ProductViewModel
+{
+    public int ProductId { get; set; }
+
+    [Required(ErrorMessage = "Please enter the product name")]
+    [StringLength(100, MinimumLength = 2)]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    [Range(0.01, 10000, ErrorMessage = "Price must be between 0.01 and 10000")]
+    public decimal Price { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    public int CategoryId { get; set; }
+
+    // Property to display the current image from the database
+    public byte[] LargeImage { get; set; }
+
+    // Property for the uploaded file
+    [Display(Name = "New Image (optional)")]
+    public IFormFile LargeImageFile { get; set; }
+}
