8a390d5a1cd61fa2e54ad396c6455a968e23f838
graccz103 <149792016+graccz103@users.noreply.github.com>
2024-10-28 21:37:58 +0100

    changes to product
    changes to product to be more complex

--- MVCSklepInternetowyNET8/Models/Product.cs
+++ MVCSklepInternetowyNET8/Models/Product.cs
@@ -10,8 +10,17 @@ public class Product
     [StringLength(100, MinimumLength = 2)]
     public string Name { get; set; }
 
+    [Required(ErrorMessage = "Please enter a description")]
+    public string Description { get; set; } // Dodano opis
+
     [Range(0.01, 10000, ErrorMessage = "Price must be between 0.01 and 10000")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
+
+    public int StockQuantity { get; set; } // Dodano ilość w magazynie
+
+    [Url(ErrorMessage = "Please enter a valid URL")]
+    public string ImageUrl { get; set; } // Opcjonalny URL obrazka
 
     [ForeignKey("Category")]
     public int CategoryId { get; set; }
