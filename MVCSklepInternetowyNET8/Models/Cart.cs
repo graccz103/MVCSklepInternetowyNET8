aa4367f8a1323042522bc435cd2b24b7559dc567
graccz103 <149792016+graccz103@users.noreply.github.com>
2024-11-06 00:21:26 +0100

    Create Cart.cs
    koszyk

---
+++ MVCSklepInternetowyNET8/Models/Cart.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Cart
+{
+    private List<CartItem> items = new List<CartItem>();
+
+    public IEnumerable<CartItem> Items => items;
+
+    public void AddToCart(Product product, int quantity)
+    {
+        var item = items.FirstOrDefault(p => p.ProductId == product.ProductId);
+        if (item == null)
+        {
+            items.Add(new CartItem
+            {
+                ProductId = product.ProductId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = quantity
+            });
+        }
+        else
+        {
+            item.Quantity += quantity;
+        }
+    }
+
+    public void RemoveFromCart(int productId)
+    {
+        var item = items.FirstOrDefault(p => p.ProductId == productId);
+        if (item != null)
+        {
+            items.Remove(item);
+        }
+    }
+
+    public decimal GetTotalPrice()
+    {
+        return items.Sum(i => i.Price * i.Quantity);
+    }
+
+    public void ClearCart()
+    {
+        items.Clear();
+    }
+}
