0d969fc16d0ce07309484a653de393ee291a6cc2
graccz103 <149792016+graccz103@users.noreply.github.com>
2024-11-06 00:50:49 +0100

    Dodanie cart, cartitem, cartcontroller i cart/index (i sklepu w layout)
    Dodanie cart, cartitem, cartcontroller i cart/index, isntalacja Newtonsoft.Json

--- MVCSklepInternetowyNET8/Models/CartItem.cs
+++ MVCSklepInternetowyNET8/Models/CartItem.cs
@@ -1,6 +1,7 @@
-﻿namespace MVCSklepInternetowyNET8.Models
+﻿public class CartItem
 {
-    public class CartItem
-    {
-    }
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
 }
